d1f9a827257fd84592b034f4b04a026bc54e973b
Felipe <felipe.olivera_@outlook.com>
2024-01-08 16:15:26 -0300

    Config docker-compose

--- src/adapters/express/server.ts
+++ src/adapters/express/server.ts
@@ -1,11 +1,18 @@
 import express from 'express';
 // import routes from './routes';
+import dotenv from 'dotenv'
+
+dotenv.config();
 
 const app = express();
-const port = 3000;
+const port = process.env.PORT;
 
 app.use(express.json());
 // app.use(routes);
+
+app.get('/', (req, res) => {
+  res.send(`Ambiente: ${process.env.NODE_ENV}`);
+});
 
 app.listen(port, () => {
   console.log(`Server is running on http://localhost:${port}`);
