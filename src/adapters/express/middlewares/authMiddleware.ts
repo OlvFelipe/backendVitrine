f1cd8804404d669e6d9db0f2d4e4849f0a9344b7
Felipe <felipe.olivera_@outlook.com>
2024-01-09 14:16:48 -0300

    Auth - Partial

--- src/adapters/express/middlewares/authMiddleware.ts
+++ src/adapters/express/middlewares/authMiddleware.ts
@@ -1,20 +1,18 @@
 import { Request, Response, NextFunction } from "express";
 import jwt, { Secret } from 'jsonwebtoken';
+import User from "../../../domain/entities/user";
 
 export const authMiddleware = (secretKey: Secret) => {
     return (req: Request, res: Response, next: NextFunction) => {
-        const token = req.headers.authorization
-
-        if (!token) {
-            return console.log('Necessário token para autenticação')
-        }
+        const user: User = req.body
+        const token = jwt.sign(user, secretKey, { expiresIn: '1h' })
 
         jwt.verify(token, secretKey, (err, decoded) => {
             if (err) {
                 return console.log('Token inválido')
             }
             (req as any).user = decoded
-
+            console.log('AUTENTICADO', token)
             next()
         })
     }
--- src/adapters/routes/userRoute.ts
+++ src/adapters/routes/userRoute.ts
@@ -1,9 +1,10 @@
 import express from 'express'
 import UserController from '../controllers/userController'
+// import { authMiddleware } from '../express/middlewares/authMiddleware'
 
 const userRouter = express.Router()
 const userController = new UserController()
-
+// const secretKey: any = process.env.JWT_SECRET
 
 userRouter.post('/users', async (req, res) => {
     await userController.createUser(req, res)
